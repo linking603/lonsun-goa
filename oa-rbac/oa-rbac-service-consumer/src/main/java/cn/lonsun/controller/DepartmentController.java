8b4a22639ce58fe356225266f9fed861f37a8546
xuzj <834122568@qq.com>
2018-05-29 18:03:28 +0800

    test

--- oa-rbac/oa-rbac-service-consumer/src/main/java/cn/lonsun/controller/DepartmentController.java
+++ oa-rbac/oa-rbac-service-consumer/src/main/java/cn/lonsun/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import cn.lonsun.service.api.DepartmentService;
 import cn.lonsun.vo.PageQueryVo;
 import cn.lonsun.vo.PaginationResultVo;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
